ff2dab2422566cc3ac603bfe1fca2442853934cd
stummk <stummk@users.noreply.github.com>
2026-03-12 17:50:57 +0100

    Added Client State changed event

---
+++ Events/ClientStateUpdated.cs
@@ -0,0 +1,14 @@
+﻿namespace Messages.Events;
+
+/// <summary>
+/// Events fires, when a client has changed.
+/// </summary>
+/// <param name="clientId">The client id, that has changed.</param>
+public class ClientStateUpdated(string clientId)
+{
+    /// <summary>
+    /// Client id.
+    /// </summary>
+    public string ClientId { get; set; } = clientId;
+
+}
